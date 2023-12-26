e4289193cdc45a7b1e3a94a036a17bbb9fd49086
Jose Reimondez <132138430+josereimondez29@users.noreply.github.com>
2023-12-26 10:06:25 +0000

    Proyecto Finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  document.querySelector("#mensaje1").innerHTML =
+    who[Math.floor(Math.random() * who.length)];
+  document.querySelector("#mensaje2").innerHTML =
+    action[Math.floor(Math.random() * action.length)];
+  document.querySelector("#mensaje3").innerHTML =
+    what[Math.floor(Math.random() * what.length)];
+  document.querySelector("#mensaje4").innerHTML =
+    when[Math.floor(Math.random() * when.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Imprimir en la consola los elementos seleccionados
+  var who1 = document.querySelector("#mensaje1").innerHTML;
+  var action1 = document.querySelector("#mensaje2").innerHTML;
+  var what1 = document.querySelector("#mensaje3").innerHTML;
+  var when1 = document.querySelector("#mensaje4").innerHTML;
+
+  console.log(who);
+  console.log(action);
+  console.log(what);
+  console.log(when);
 };
